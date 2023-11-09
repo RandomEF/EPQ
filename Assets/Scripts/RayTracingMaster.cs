949d307610138d3bfe785ae78eedbec326a094d7
RandomEF <eshan4u@outlook.com>
2023-11-09 13:26:24 +0000

    Reflection

--- Assets/Scripts/RayTracingMaster.cs
+++ Assets/Scripts/RayTracingMaster.cs
@@ -6,11 +6,20 @@ public class RayTracingMaster : MonoBehaviour
     private RenderTexture target;
     public Texture SkyboxTexture;
     private Camera _camera;
+    private uint _currentSample = 0;
+    private Material _addMaterial;
 
     private void Awake(){
         _camera = GetComponent<Camera>();
     }
     
+    private void Update() {
+        if (transform.hasChanged){
+            _currentSample = 0;
+            transform.hasChanged = false;
+        }
+    }
+
     private void InitRenderTexture(){
         if (target == null || target.width != Screen.width || target.height != Screen.height){
             if (target != null){
@@ -19,6 +28,7 @@ public class RayTracingMaster : MonoBehaviour
             
             target = new RenderTexture(Screen.width, Screen.height, 0, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
             target.enableRandomWrite = true;
+            _currentSample = 0;
             target.Create();
         }
     }
@@ -31,7 +41,12 @@ public class RayTracingMaster : MonoBehaviour
         int threadGroupsY = Mathf.CeilToInt(Screen.height / 8f);
         RayTracingShader.Dispatch(0, threadGroupsX, threadGroupsY, 1);
 
-        Graphics.Blit(target, dest);
+        if (_addMaterial == null){
+            _addMaterial = new Material(Shader.Find("Hidden/AddShader"));
+        }
+        _addMaterial.SetFloat("_Sample", _currentSample);
+        Graphics.Blit(target, dest, _addMaterial);
+        _currentSample++;
     }
     
     private void OnRenderImage(RenderTexture src, RenderTexture dest) {
@@ -43,5 +58,6 @@ public class RayTracingMaster : MonoBehaviour
         RayTracingShader.SetMatrix("_CameraToWorld", _camera.cameraToWorldMatrix);
         RayTracingShader.SetMatrix("_CameraInverseProjection", _camera.projectionMatrix.inverse);
         RayTracingShader.SetTexture(0, "_SkyboxTexture", SkyboxTexture);
+        RayTracingShader.SetVector("_PixelOffset", new Vector2(Random.value, Random.value));
     }
 }
